064eab6d86b616d10fe3231113f99178260b8f56
emi.sasaki <emi.sasaki@rakuten.com>
2018-06-05 04:32:49 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,3 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::get('/', 'TasklistsController@index');
-
-Route::resource('tasklists', 'TasklistsController');
